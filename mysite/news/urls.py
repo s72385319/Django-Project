dae6da374b23290350a616fc4bc785505a6babc7
СТАСЕК <stasberchun@gmail.com>
2025-11-15 20:46:36 +0200

    commit

---
+++ mysite/news/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.news, name="news"),
+    path('create', views.create, name="create"),
+    path('<int:pk>', views.NewsDetailView.as_view(), name="newsdetail")
+]
